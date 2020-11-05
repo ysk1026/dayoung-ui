98153755e96056fa9f2e832c225c0c8184aa3862
Kim <fkqoseka@gmail.com>
2020-11-05 19:11:11 +0900

    appbar

--- src/templates/SearchAppBar.jsx
+++ src/templates/SearchAppBar.jsx
@@ -4,9 +4,12 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import {Nav} from '../components'
+import {Link} from 'react-router-dom'
+import AccountCircle from '@material-ui/icons/AccountCircle';
 // import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
 // import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
@@ -21,12 +24,20 @@ const useStyles = makeStyles((theme) => ({
   },
 
   title: {
-    flexGrow: 1,
     display: 'none',
+    color: 'black',
+    // textDecoration: 'none',
     [theme.breakpoints.up('sm')]: {
-      display: 'block',
+      display: 'inline-block',
     },
   },
+
+  link: {
+    fontsize: '1 rem',
+    textDecoration: 'none',
+    color: 'black'
+  },
+
   search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
@@ -84,10 +95,35 @@ export default function SearchAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Review
+            <Link to="/home" className={classes.link}>다영이</Link>
           </Typography>
-          <Typography>
-              <Nav/>
+          <Typography style = {{paddingLeft: 20}}>
+          <Button>
+            <Link to="/" className={classes.link}>회원 가입</Link>
+          </Button>
+          <Button>
+            <Link to="/" className={classes.link}>로그인</Link>
+          </Button>
+          <Button>
+            <Link to="/" className={classes.link}>배우</Link>
+          </Button>
+          <Button>
+            <Link to="/" className={classes.link}>리뷰</Link>
+          </Button>
+          <Button>
+            <Link to="/" className={classes.link}>영화 별점</Link>
+          </Button>
+          <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <AccountCircle>
+            <Link to="/" className={classes.link}></Link>
+                </AccountCircle>                
+              </IconButton>
+
           </Typography>
           {/* <div className={classes.search}>
             <div className={classes.searchIcon}>
